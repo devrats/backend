4037ca17ad8e37bd170a2a69d3fd8900086009d8
devrats <devvratsharma9759@gmail.com>
2025-04-23 11:41:36 +0530

    mongo cluster added

--- connection.js
+++ connection.js
@@ -1,7 +1,7 @@
 
 import mongoose from 'mongoose';
 export const connectToDB = async () => {
-  mongoose.connect("mongodb://localhost:27017/dsaSheet", {
+  mongoose.connect("mongodb+srv://abcd:devvrat@cluster0.eysyq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
